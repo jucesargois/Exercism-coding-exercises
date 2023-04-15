bb455feb7ca730f74c6c35daf953b8f872b4d322
Julio Cesar Gois <jucesargois@gmail.com>
2023-04-15 16:48:32 -0300

    inserted code in function remainingMinutesInOven and created variable EXPECTED_MINUTES_IN_OVEN

--- javascript/lasagna/lasagna.js
+++ javascript/lasagna/lasagna.js
@@ -34,6 +34,7 @@
  * The number of minutes it takes to prepare a single layer.
  */
 const PREPARATION_MINUTES_PER_LAYER = 2;
+export const EXPECTED_MINUTES_IN_OVEN = 40;
 
 /**
  * Determines the number of minutes the lasagna still needs to remain in the
@@ -43,7 +44,8 @@ const PREPARATION_MINUTES_PER_LAYER = 2;
  * @returns {number} the number of minutes remaining
  */
 export function remainingMinutesInOven(actualMinutesInOven) {
-  throw new Error('Remove this line and implement the function');
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven
+  
 }
 
 /**
