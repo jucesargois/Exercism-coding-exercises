1a581257a9d42a1233823e49e93c5e032e36060c
Julio Cesar Gois <jucesargois@gmail.com>
2023-04-22 00:18:27 -0300

    inserted logic for return values in functions insertItemAtBottom, removeItemAtBottom and checkSizeOfStack

--- javascript/elyses-enchantments/enchantments.js
+++ javascript/elyses-enchantments/enchantments.js
@@ -74,7 +74,8 @@ export function removeItemFromTop(cards) {
  * @returns {number[]} the cards including the new card
  */
 export function insertItemAtBottom(cards, newCard) {
-  throw new Error('Implement the insertItemAtBottom function');
+  cards.unshift(newCard)
+  return cards
 }
 
 /**
@@ -85,7 +86,8 @@ export function insertItemAtBottom(cards, newCard) {
  * @returns {number[]} the cards without the removed card
  */
 export function removeItemAtBottom(cards) {
-  throw new Error('Implement the removeItemAtBottom function');
+  cards.shift()
+  return cards
 }
 
 /**
@@ -97,5 +99,5 @@ export function removeItemAtBottom(cards) {
  * @returns {boolean} true if there are exactly stackSize number of cards, false otherwise
  */
 export function checkSizeOfStack(cards, stackSize) {
-  throw new Error('Implement the checkSizeOfStack function');
+  return cards.length === stackSize
 }
