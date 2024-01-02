99642f45da8a19704cbdd9fb20dcd49d86a75363
jucesargois <jucesargois@gmail.com>
2024-01-02 18:36:09 -0300

    function chooseVehicle

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -1,4 +1,4 @@
-// @ts-check
+// @ts-nocheck
 //
 // The line above enables type checking for this file. Various IDEs interpret
 // the @ts-check directive. It will give you helpful autocompletion when
@@ -23,8 +23,16 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  
-  throw new Error('Please implement the chooseVehicle function');
+  let car1 = option1.split('')
+  let car2 = option2.split('')
+
+  let op1 = car1.find(i => isNaN(Number(i)))
+  let op2 = car2.find(i => isNaN(Number(i)))
+  if (op1[0].charCodeAt(0) < op2[0].charCodeAt(0)) {
+    return `${option1} is clearly the better choice.`
+  } else if (op2[0].charCodeAt(0) < op1[0].charCodeAt(0)) {
+    return `${option2} is clearly the better choice.`
+  }
 }
 
 /**
@@ -35,6 +43,7 @@ export function chooseVehicle(option1, option2) {
  * @param {number} age
  * @returns {number} expected resell price in the dealership
  */
+// @ts-ignore
 export function calculateResellPrice(originalPrice, age) {
   throw new Error('Please implement the calculateResellPrice function');
 }
