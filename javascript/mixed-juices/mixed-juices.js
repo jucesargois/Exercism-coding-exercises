8f12820bdb069d7e98f618d09dd4b7e646819bfc
jucesargois <jucesargois@gmail.com>
2024-02-03 23:29:43 -0300

    implemented logic in func: timeToMixJuice

--- javascript/mixed-juices/mixed-juices.js
+++ javascript/mixed-juices/mixed-juices.js
@@ -1,4 +1,4 @@
-// @ts-check
+// @ts-nocheck
 //
 // The line above enables type checking for this file. Various IDEs interpret
 // the @ts-check directive. It will give you helpful autocompletion when
@@ -11,7 +11,22 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Please implement the timeToMixJuice function');
+  switch (name) {
+    case "Pure Strawberry Joy":
+      return 0.5
+    case "Energizer":
+      return 1.5
+    case "Green Garden":
+      return 1.5
+
+    case "Tropical Island":
+      return 3
+
+    case "All or Nothing":
+      return 5
+    default:
+      return 2.5
+  }
 }
 
 /**
